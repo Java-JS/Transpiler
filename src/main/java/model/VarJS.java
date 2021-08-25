4be36e0d3b0bbf098a2bf3cb799d3ad4063a03f5
LucasMR3 <lucas_mr@live.com>
2021-08-25 20:14:44 -0300

    VarJS class added

--- src/main/java/model/VarJS.java
+++ src/main/java/model/VarJS.java
@@ -1,4 +1,11 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class VarJS {
+    private final String name;
+    private final String allocation;
 }
