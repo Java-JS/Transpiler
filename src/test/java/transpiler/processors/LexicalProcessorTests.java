8a9c0c708cc63c10719ab6afc0b4d8dc033a4d71
LucasMR3 <lucas_mr@live.com>
2021-10-06 20:18:28 -0300

    Tests improved with 2 new tests

--- src/test/java/transpiler/processors/LexicalProcessorTests.java
+++ src/test/java/transpiler/processors/LexicalProcessorTests.java
@@ -9,19 +9,34 @@ class LexicalProcessorTests {
     LexicalProcessor lexicalProcessor = new LexicalProcessor();
 
     @Test
-    void helloWorld() {
+    void testHelloWorldTokens() {
         Assertions.assertEquals(List.of("System.out.println",
                         "(",
                         "\"Hello World!\"",
                         ")",
                         ";"),
                 lexicalProcessor.interactOverLine("System.out.println(\"Hello World!\");"));
+    }
 
+    @Test
+    void testHelloWorldFragments(){
         Assertions.assertEquals(List.of("[FRAG: print=System.out.println]",
                         "[FRAG: parenthesis=(]",
                         "[FRAG: String found=\"Hello World!\"]",
                         "[FRAG: parenthesis=)]",
                         "[FRAG: semicolon=;]"),
                 lexicalProcessor.interact(List.of("System.out.println(\"Hello World!\");")));
+    }
+
+    @Test
+    void testStringWithPlusSymbol(){
+        Assertions.assertEquals(List.of("[FRAG: print=System.out.println]",
+                        "[FRAG: parenthesis=(]",
+                        "[FRAG: String found=\"Hello World!\"]",
+                        "[FRAG: operation=+]",
+                        "[FRAG: String found=\"random\"]",
+                        "[FRAG: parenthesis=)]",
+                        "[FRAG: semicolon=;]"),
+                lexicalProcessor.interact(List.of("System.out.println(\"Hello World!\" + \"random\");")));
     }
 }
