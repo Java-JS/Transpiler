ccba997b016c20438b2b2ce2cb261b1afd66b6ed
LucasMR3 <lucas_mr@live.com>
2021-10-06 20:14:57 -0300

    SemanticProcessor class added

---
+++ src/main/java/transpiler/processors/SemanticProcessor.java
@@ -0,0 +1,10 @@
+package transpiler.processors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SemanticProcessor {
+    private final List<String> execution = new ArrayList<>();
+
+}
