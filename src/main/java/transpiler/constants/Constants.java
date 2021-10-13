58d08d657769c57ab9150bb7fc8d350d1b27845f
LucasMR3 <lucas_mr@live.com>
2021-10-13 19:46:18 -0300

    new constants added

--- src/main/java/transpiler/constants/Constants.java
+++ src/main/java/transpiler/constants/Constants.java
@@ -3,4 +3,15 @@ package transpiler.constants;
 public class Constants {
     public static final String UNEXPECTED = "unexpected";
     public static final String CLASS = "class";
+    public static final String PARENTHESIS = "parenthesis";
+    public static final String CURLY_BRACKETS = "curly_brackets";
+    public static final String SQUARE_BRACKETS = "square_brackets";
+    public static final String PRINT = "print";
+    public static final String SEMICOLON = "semicolon";
+    public static final String NUMBER = "number";
+    public static final String COMMAND = "command";
+    public static final String TYPE = "type";
+    public static final String SPACE = "space";
+    public static final String OPERATION = "operation";
+    public static final String ACCESSOR_MODIFIER = "accessor_modifier";
 }
