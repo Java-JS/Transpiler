411fabe77416af73a860e19cdb7000c6659ca197
LucasMR3 <lucas_mr@live.com>
2021-10-31 22:17:05 -0300

    Lexeme domain added

---
+++ src/main/java/transpiler/domain/Lexeme.java
@@ -0,0 +1,19 @@
+package transpiler.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import transpiler.enumerator.LexemeType;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Lexeme {
+    LexemeType type;
+    String command;
+
+    @Override
+    public String toString() {
+        return "Lexeme[" + type.getLexemeType() + ", " + command + ']';
+    }
+}
