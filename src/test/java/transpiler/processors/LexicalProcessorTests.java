245c97801672405fc55cbe544987cd11bfb7fbf8
LucasMR3 <lucas_mr@live.com>
2021-10-31 22:31:09 -0300

    changed to work with LexemeType

--- src/test/java/transpiler/processors/LexicalProcessorTests.java
+++ src/test/java/transpiler/processors/LexicalProcessorTests.java
@@ -2,11 +2,10 @@ package transpiler.processors;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import transpiler.model.Lexeme;
+import transpiler.domain.Lexeme;
+import transpiler.enumerator.LexemeType;
 
 import java.util.List;
-
-import static transpiler.constants.Constants.*;
 
 class LexicalProcessorTests {
     LexicalProcessor lexicalProcessor = new LexicalProcessor();
@@ -24,24 +23,24 @@ class LexicalProcessorTests {
     @Test
     void testHelloWorldFragments() {
         Assertions.assertEquals(List.of(
-                        Lexeme.builder().type(PRINT).command("System.out.println").build(),
-                        Lexeme.builder().type(PARENTHESIS).command("(").build(),
-                        Lexeme.builder().type(STRING_FOUND).command("\"Hello World!\"").build(),
-                        Lexeme.builder().type(PARENTHESIS).command(")").build(),
-                        Lexeme.builder().type(SEMICOLON).command(";").build()),
+                        Lexeme.builder().type(LexemeType.PRINT).command("System.out.println").build(),
+                        Lexeme.builder().type(LexemeType.PARENTHESIS).command("(").build(),
+                        Lexeme.builder().type(LexemeType.STRING_FOUND).command("\"Hello World!\"").build(),
+                        Lexeme.builder().type(LexemeType.PARENTHESIS).command(")").build(),
+                        Lexeme.builder().type(LexemeType.SEMICOLON).command(";").build()),
                 lexicalProcessor.interact(List.of("System.out.println(\"Hello World!\");")));
     }
 
     @Test
     void testStringWithPlusSymbol() {
         Assertions.assertEquals(List.of(
-                        Lexeme.builder().type(PRINT).command("System.out.println").build(),
-                        Lexeme.builder().type(PARENTHESIS).command("(").build(),
-                        Lexeme.builder().type(STRING_FOUND).command("\"Hello World!\"").build(),
-                        Lexeme.builder().type(OPERATION).command("+").build(),
-                        Lexeme.builder().type(STRING_FOUND).command("\"random\"").build(),
-                        Lexeme.builder().type(PARENTHESIS).command(")").build(),
-                        Lexeme.builder().type(SEMICOLON).command(";").build()),
+                        Lexeme.builder().type(LexemeType.PRINT).command("System.out.println").build(),
+                        Lexeme.builder().type(LexemeType.PARENTHESIS).command("(").build(),
+                        Lexeme.builder().type(LexemeType.STRING_FOUND).command("\"Hello World!\"").build(),
+                        Lexeme.builder().type(LexemeType.OPERATION).command("+").build(),
+                        Lexeme.builder().type(LexemeType.STRING_FOUND).command("\"random\"").build(),
+                        Lexeme.builder().type(LexemeType.PARENTHESIS).command(")").build(),
+                        Lexeme.builder().type(LexemeType.SEMICOLON).command(";").build()),
                 lexicalProcessor.interact(List.of("System.out.println(\"Hello World!\" + \"random\");")));
     }
 }
