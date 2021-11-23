df4e17aa039b6f4d62cbee386a7d63a4e8b437c7
LucasMR3 <lucas_mr@live.com>
2021-11-22 22:34:06 -0300

    structure changes

--- src/main/java/transpiler/processors/SemanticProcessor.java
+++ src/main/java/transpiler/processors/SemanticProcessor.java
@@ -1,13 +1,11 @@
 package transpiler.processors;
 
+import transpiler.domain.JavaVariable;
 import transpiler.domain.Lexeme;
 import transpiler.enumerator.LexemeType;
-import transpiler.domain.JavaVariable;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static transpiler.constants.Constants.*;
 
 public class SemanticProcessor {
 
@@ -15,23 +13,44 @@ public class SemanticProcessor {
         List<String> execution = new ArrayList<>();
         List<JavaVariable> variables = new ArrayList<>();
 
+        boolean insideClass = false;
+
+        System.out.println();
+
         for (int i = 0; i < lexemes.size(); i++) {
 
-            if (lexemes.get(i).getType().equals(LexemeType.CLASS)) {
-                if (i >= 1 && lexemes.get(i - 1).getCommand().equals("public")) {
-                    if (lexemes.get(i + 1).getType().equals(LexemeType.UNEXPECTED)) {
-                        System.out.println("Class found!");
-                        System.out.println(lexemes.get(i));
 
+            switch (lexemes.get(i).getType()) {
+
+                case CLASS -> {
+
+                    if (i >= 1 && lexemes.get(i - 1).getCommand().equals("public")) {
+                        if (lexemes.get(i + 1).getType().equals(LexemeType.UNEXPECTED)) {
+                            System.out.println(lexemes.get(i));
+                            insideClass = true;
+
+                        }
                     }
                 }
+
+
+                case TYPE -> {
+
+                    if (lexemes.get(i + 1).getType().equals(LexemeType.UNEXPECTED)) {
+                        System.out.println(lexemes.get(i));
+                    }
+
+                    if (lexemes.get(i + 1).getCommand().equals("[") && lexemes.get(i + 2).getCommand().equals("]")) {
+                        if (lexemes.get(i + 3).getType().equals(LexemeType.UNEXPECTED)) {
+                            System.out.println(lexemes.get(i + 3));
+                        }
+                    }
+
+
+                }
+
             }
 
-            if (lexemes.get(i).getType() != LexemeType.TYPE) {
-                if (i >= 1 && lexemes.get(i - 1).getType().equals(LexemeType.CLASS)) {
-                    System.out.println(lexemes.get(i));
-                }
-            }
         }
 
         // TODO check if the variable was already declared or not declared
@@ -41,6 +60,7 @@ public class SemanticProcessor {
 //        System.out.println(execution);
 
     }
+
 
     public static void main(String[] args) {
         List<Lexeme> test = new ArrayList<>();
